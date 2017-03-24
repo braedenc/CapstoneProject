16bf10e3bf22a0b4226cbb71c14ba145b7272709
braedenc <braedenchriske@gmail.com>
2017-03-24 09:44:17 -0500

    fixed some views and added user input text boxes on weather page

---
+++ Vacay/Vacay/Models/ViewModels/WeatherViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vacay.Models.ViewModels
+{
+    public class WeatherViewModels
+    {
+        public string City { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
